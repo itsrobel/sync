c91839896125e0b8575bad739152f7319cdce3f6
Robel Schwarz <itsrobel.schwarz@gmail.com>
2024-12-07 19:24:06 -0800

    client server adapters

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -12,10 +12,29 @@ import (
 	ft "github.com/itsrobel/sync/internal/services/filetransfer"
 	"github.com/itsrobel/sync/internal/services/filetransfer/filetransferconnect"
 	ct "github.com/itsrobel/sync/internal/types"
+	"github.com/itsrobel/sync/internal/watcher"
 )
 
+type FileTransferClient struct {
+	filetransferconnect.FileServiceClient
+}
+
 func main() {
-	filePath := "example.txt"
+	Client()
+}
+
+func Client() {
+	conn := filetransferconnect.NewFileServiceClient(http.DefaultClient, "http://localhost:50051")
+	filetransfer := &FileTransferClient{conn}
+
+	// watcher.WatchFiles(ct.Directory)
+	//
+	//
+	client := NewWatcherClient(*filetransfer)
+	client.watcher.Watch("/path/to/watch")
+}
+
+func (client *FileTransferClient) UploadFile(filePath string) {
 	file, openErr := os.Open(filePath)
 	id := 1
 	if openErr != nil {
@@ -25,15 +44,13 @@ func main() {
 	defer file.Close()
 	buf := make([]byte, ct.ChunkSize) // Define your buffer size
 
-	client := filetransferconnect.NewFileServiceClient(http.DefaultClient, "http://localhost:50051")
 	stream := client.SendFileToServer(context.Background())
 
 	for {
 
 		log.Printf("Trying to upload...")
 		n, readErr := file.Read(buf) // Read from file into buffer
-
-		if n > 0 { // Only send if there's data to send
+		if n > 0 {                   // Only send if there's data to send
 			fileData := &ft.FileData{
 				Id:       fmt.Sprintf("%d", id),
 				Location: filepath.Base(filePath), // Use actual filename here
@@ -62,4 +79,50 @@ func main() {
 		}
 
 	}
+}
+
+func saveToFile(filename string, data []byte) error {
+	path, _ := filepath.Abs(fmt.Sprintf("./%s", ct.Directory))
+	filePath := filepath.Join(path, filename)
+
+	file, openErr := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if openErr != nil {
+		log.Printf("Failed to open local file %s: %v", filename, openErr)
+		return openErr
+	}
+	defer file.Close()
+
+	if _, writeErr := file.Write(data); writeErr != nil {
+		log.Printf("Failed writing data to local file %s: %v", filename, writeErr)
+		return writeErr
+	}
+
+	log.Printf("Successfully saved data to %s", filename)
+	return nil
+}
+
+type WatcherClient struct {
+	grpcClient FileTransferClient
+	watcher    *watcher.FileWatcher
+}
+
+// gprc client call
+func (c *WatcherClient) HandleChange(path string, eventType string) error {
+	// Send gRPC notification to server
+	// _, err := c.grpcClient.NotifyFileChange(context.Background(), &FileChangeRequest{
+	//     Path:      path,
+	//     EventType: eventType,
+	// })
+	// return err
+	return nil
+}
+
+func NewWatcherClient(grpcClient FileTransferClient) *WatcherClient {
+	watcher, _ := watcher.NewFileWatcher()
+	client := &WatcherClient{
+		grpcClient: grpcClient,
+		watcher:    watcher,
+	}
+	client.watcher.AddHandler(client)
+	return client
 }
--- internal/watcher/watcher.go
+++ internal/watcher/watcher.go
@@ -1,67 +1,129 @@
 package watcher
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/fsnotify/fsnotify"
-	"github.com/itsrobel/sync/internal/datacontroller"
 )
 
-// Watch files and notify all connected clients when a file changes.
-//
-// if the client cant connect to the server, it should watch the files in the directory and send the files to the server when the server is back online
-func WatchFiles(path string) {
-	client, _ := datacontroller.ConnectMongo()
+type WatcherNotification interface {
+	HandleChange(path string, eventType string) error
+}
+
+type FileWatcher struct {
+	watcher  *fsnotify.Watcher
+	handlers []WatcherNotification
+}
+
+func NewFileWatcher() (*FileWatcher, error) {
 	watcher, err := fsnotify.NewWatcher()
-	collection := client.Database("sync").Collection("server")
-	documents, _ := datacontroller.GetAllDocuments(collection)
-
 	if err != nil {
-		return
+		return nil, err
 	}
+	return &FileWatcher{
+		watcher:  watcher,
+		handlers: make([]WatcherNotification, 0),
+	}, nil
+}
 
-	// deletedCount, _ := deleteAllDocuments(collection)
-	// fmt.Printf("Deleted %d documents\n", deletedCount)
-	for _, doc := range documents {
-		fmt.Println("documents:", doc)
-	}
-
-	defer watcher.Close()
-
+func (fw *FileWatcher) Watch(path string) error {
+	done := make(chan bool)
 	go func() {
 		for {
 			select {
-			case event, ok := <-watcher.Events:
-				if !ok {
-					return
+			case event := <-fw.watcher.Events:
+				for _, handler := range fw.handlers {
+					eventType := getEventType(event.Op)
+					handler.HandleChange(event.Name, eventType)
 				}
-
-				if (event.Op == fsnotify.Create) && datacontroller.ValidFileExtension(event.Name) {
-					log.Println("valid event location:", event)
-					fileID := datacontroller.CreateFile(collection, event.Name)
-
-					datacontroller.CreateFileVersion(collection, fileID)
-
-				}
-
-				if event.Op&fsnotify.Write == fsnotify.Write {
-					log.Println("Modified or created file:", event.Name)
-				}
-
-			case err, ok := <-watcher.Errors:
-				if !ok {
-					return
-				}
-				log.Println("Error:", err)
+			case err := <-fw.watcher.Errors:
+				log.Printf("Error: %v", err)
 			}
 		}
 	}()
 
-	err = watcher.Add(path)
+	err := fw.watcher.Add(path)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	fmt.Printf("Watching directory: %s\n", path)
-	<-make(chan struct{})
+	<-done
+	return nil
 }
+
+func getEventType(op fsnotify.Op) string {
+	switch {
+	case op&fsnotify.Create == fsnotify.Create:
+		return "CREATE"
+	case op&fsnotify.Write == fsnotify.Write:
+		return "WRITE"
+	case op&fsnotify.Remove == fsnotify.Remove:
+		return "REMOVE"
+	case op&fsnotify.Rename == fsnotify.Rename:
+		return "RENAME"
+	case op&fsnotify.Chmod == fsnotify.Chmod:
+		return "CHMOD"
+	default:
+		return "UNKNOWN"
+	}
+}
+
+func (fw *FileWatcher) AddHandler(handler WatcherNotification) {
+	fw.handlers = append(fw.handlers, handler)
+}
+
+// Watch files and notify all connected clients when a file changes.
+// if the client cant connect to the server, it should watch the files in the directory and send the files to the server when the server is back online
+// func WatchFiles(path string) {
+// 	client, _ := datacontroller.ConnectMongo()
+// 	watcher, err := fsnotify.NewWatcher()
+// 	collection := client.Database("sync").Collection("server")
+// 	documents, _ := datacontroller.GetAllDocuments(collection)
+//
+// 	if err != nil {
+// 		return
+// 	}
+//
+// 	// deletedCount, _ := deleteAllDocuments(collection)
+// 	// fmt.Printf("Deleted %d documents\n", deletedCount)
+// 	for _, doc := range documents {
+// 		fmt.Println("documents:", doc)
+// 	}
+//
+// 	defer watcher.Close()
+//
+// 	go func() {
+// 		for {
+// 			select {
+// 			case event, ok := <-watcher.Events:
+// 				if !ok {
+// 					return
+// 				}
+//
+// 				if (event.Op == fsnotify.Create) && datacontroller.ValidFileExtension(event.Name) {
+// 					log.Println("valid event location:", event)
+// 					fileID := datacontroller.CreateFile(collection, event.Name)
+//
+// 					datacontroller.CreateFileVersion(collection, fileID)
+//
+// 				}
+//
+// 				if event.Op&fsnotify.Write == fsnotify.Write {
+// 					log.Println("Modified or created file:", event.Name)
+// 				}
+//
+// 			case err, ok := <-watcher.Errors:
+// 				if !ok {
+// 					return
+// 				}
+// 				log.Println("Error:", err)
+// 			}
+// 		}
+// 	}()
+//
+// 	err = watcher.Add(path)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	fmt.Printf("Watching directory: %s\n", path)
+// 	<-make(chan struct{})
+// }
