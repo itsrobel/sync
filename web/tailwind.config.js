a99e43914baefe9c91b987f58599cf33a53faf90
Robel Schwarz <itsrobel.schwarz@gmail.com>
2025-01-29 09:08:23 +0000

    daisyui 5 beta update

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,21 +1,11 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./internal/templates/**/*.templ"],
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [require("@tailwindcss/typography")],
   theme: {
     fontFamily: {
-      'excali': ["Excalifont", "sans-serif"],
-      'mono': ["SFMono-Regular", 'jetbrains mono', "monospace"],
+      excali: ["Excalifont", "sans-serif"],
+      mono: ["SFMono-Regular", "jetbrains mono", "monospace"],
     },
   },
-  daisyui: {
-    themes: ["wireframe", "dim"],
-    base: true,
-    styled: true,
-    utils: true,
-    prefix: "",
-    themeRoot: ":root",
-  },
-  darkMode: ["selector", '[data-theme="dim"]'],
 };
